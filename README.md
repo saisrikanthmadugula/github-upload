3d6c491439090fcf69b16e9687febc3ee5e6afb2
saisrikanthmadugula <saisrikanthmadugula@gmail.com>
2020-09-30 13:36:49 +0530

    commit on 30th sep

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # github-upload
 This is my first Rust Program - "Hello, World!"
+Updated on 30th Sep, 2020.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
     println!("Hello, world from saisrikanthmadugula!");
+    println!("Updated on 30th Sep,2020");
 }
